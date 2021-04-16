f63919892e5cfdfb82e9294392c75418bfb8831f
CrazyJ1984 <jennifer.holba@yahoo.com>
2021-04-16 15:09:43 -0500

    Initial commit

---
+++ src/main/java/crazyj/crazycountrycooking/common/CrazyCountryCookingMod.java
@@ -0,0 +1,20 @@
+package crazyj.crazycountrycooking.common;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = CrazyCountryCookingMod.MODID, version = CrazyCountryCookingMod.VERSION)
+public class CrazyCountryCookingMod
+{
+    public static final String MODID = "examplemod";
+    public static final String VERSION = "1.0";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+		// some example code
+        System.out.println("DIRT BLOCK >> "+Blocks.dirt.getUnlocalizedName());
+    }
+}
